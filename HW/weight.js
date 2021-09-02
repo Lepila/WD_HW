28a68dadee82b1ae5ed93c9bbd73239fa4478bb6
Mila <86366895+Lepila@users.noreply.github.com>
2021-09-02 18:39:56 +0300

    Add files via upload

---
+++ HW/weight.js
@@ -0,0 +1,18 @@
+let h;
+let w;
+BMI= w/ h*h;
+if(BMI< 18,5)
+{
+console.log("Вашето тегло е поднормено")
+}
+else if (18,5< BMI > 24,99)
+{
+console.log("Вашето тегло е нормално тегло")
+}
+else if (BMI >= 25,0)
+{
+console.log("Вашето тегло е наднормено тегло")
+}
+else {
+console.log("Вие сте затлъстял")
+}
